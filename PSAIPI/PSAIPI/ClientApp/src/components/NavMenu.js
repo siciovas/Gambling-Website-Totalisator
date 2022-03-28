5cc40bfdfaccc8312fef162f9cd682d5fb46aad5
Rokas Sičiovas <roksic@ktu.lt>
2022-03-28 19:46:10 +0300

    added login page and register (not fully)

--- PSAIPI/PSAIPI/ClientApp/src/components/NavMenu.js
+++ PSAIPI/PSAIPI/ClientApp/src/components/NavMenu.js
@@ -34,10 +34,10 @@ export class NavMenu extends Component {
                   <NavLink tag={Link} className="text-dark" to="/">Home</NavLink>
                 </NavItem>
                 <NavItem>
-                  <NavLink tag={Link} className="text-dark" to="/counter">Counter</NavLink>
+                  <NavLink tag={Link} className="text-dark" to="/login-page">Login</NavLink>
                 </NavItem>
                 <NavItem>
-                  <NavLink tag={Link} className="text-dark" to="/fetch-data">Fetch data</NavLink>
+                  <NavLink tag={Link} className="text-dark" to="/register-page">Register</NavLink>
                 </NavItem>
               </ul>
             </Collapse>
