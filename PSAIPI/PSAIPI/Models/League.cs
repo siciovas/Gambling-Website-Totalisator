c4bd2fce6e7d44db3dfc1284e95be1994e608e8d
Kristupas Lozoraitis <kristupas.lozoraitis@ktu.edu>
2022-04-05 00:08:06 +0300

    First migration

--- PSAIPI/PSAIPI/Models/League.cs
+++ PSAIPI/PSAIPI/Models/League.cs
@@ -1,7 +1,10 @@
-﻿namespace PSAIPI.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PSAIPI.Models
 {
     public class League
     {
+        [Key]
         public int Id { get; set; }
         public string Title { get; set; } = string.Empty;
         public string Description { get; set; } = string.Empty;
